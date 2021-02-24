73f4bc35f67d56294fe4ac6479f7268f2047eddc
Geoff Woodburn <geoff@woodburndesigns.com>
2021-02-24 14:41:45 -0800

    Allow arrays or strings and handle appropriately

--- steps/email-send/step.py
+++ steps/email-send/step.py
@@ -5,23 +5,26 @@ from nebula_sdk import Interface, Dynamic as D
 
 relay = Interface()
 
+def coerce_to_list(data):
+  return data if isinstance(data, list) else [data]
+
 api_key = relay.get(D.sendgrid.connection.apiKey)
 
-fromAddress = relay.get("from")
-toAddress = relay.get(D.to).split(",")
+from_address = relay.get("from")
 subject = relay.get(D.subject)
+to_address = coerce_to_list(relay.get(D.to))
 
-ccAddresses = None
+cc_addresses = None
 
 try:
-  ccAddresses = relay.get(D.cc).split(",")
+  cc_addresses = coerce_to_list(relay.get(D.cc))
 except:
   pass
 
-bccAddresses = None
+bcc_addresses = None
 
 try:
-  bccAddresses = relay.get(D.bcc).split(",")
+  bcc_addresses = coerce_to_list(relay.get(D.bcc))
 except:
   pass
 
@@ -40,34 +43,34 @@ except:
   pass
 
 message = Mail(
-    from_email=fromAddress,
-    to_emails=toAddress,
+    from_email=from_address,
+    to_emails=to_address,
     subject=subject,
     plain_text_content=text,
     html_content=html)
 
-if ccAddresses is not None:
+if cc_addresses is not None:
   ccs = []
 
-  for ccAddress in ccAddresses:
-    ccs.append(Cc(ccAddress))
+  for cc_address in cc_addresses:
+    ccs.append(Cc(cc_address))
     
   message.cc = ccs
 
-if bccAddresses is not None:
+if bcc_addresses is not None:
   bccs = []
 
-  for bccAddress in bccAddresses:
-    bccs.append(Bcc(bccAddress))
+  for bcc_address in bcc_addresses:
+    bccs.append(Bcc(bcc_address))
 
   message.bcc = bccs
 
 try:
     sg = SendGridAPIClient(api_key)
     response = sg.send(message)
-    toAddressString = ', '.join(toAddress)
+    to_addressString = ', '.join(to_address)
 
-    print(f'Your email titled "{subject}" was sent successfully to: {toAddressString}' )
+    print(f'Your email titled "{subject}" was sent successfully to: {to_addressString}' )
 except Exception as e:
     print(str(e))
     exit(1)
